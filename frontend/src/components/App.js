fbe54ec4e9e41d6c37a637fc730bcfe12bc792b6
vpanacheva <v_panacheva@mail.ru>
2023-08-15 09:55:23 +0500

    8

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -79,7 +79,8 @@ function App() {
   /** проверка токена */
   useEffect(() => {
     handleTokenCheck()
-  }, [handleTokenCheck])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useEffect(() => {
     if (isLoggedIn) {
